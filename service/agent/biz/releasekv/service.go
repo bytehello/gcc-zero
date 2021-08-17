b51d4ea607c26a5322113424ce5e4cd162be3049
abyte <gechanghang@gmail.com>
2021-08-18 00:08:50 +0800

    变量

--- service/agent/biz/releasekv/service.go
+++ service/agent/biz/releasekv/service.go
@@ -3,12 +3,24 @@ package releasekv
 import (
 	"fmt"
 	"github.com/bytehello/gcc-zero/common/file"
+	"github.com/tal-tech/go-zero/core/logx"
+	"github.com/tal-tech/go-zero/core/syncx"
+	"github.com/tal-tech/go-zero/zrpc"
+	"io/ioutil"
 	"os"
 	"path/filepath"
+	"sync"
+)
+
+const (
+	rpcClientPrefix = "gcc-zero:agent:rpc:client:"
 )
 
 type Service struct {
 	releasePath string
+	resource    map[string]zrpc.Client
+	shareCall   syncx.SharedCalls
+	lock        sync.RWMutex
 }
 
 func NewService(path string) *Service {
@@ -19,26 +31,64 @@ func NewService(path string) *Service {
 	}
 	return &Service{
 		releasePath: path,
+		resource:    make(map[string]zrpc.Client),
+		shareCall:   syncx.NewSharedCalls(),
 	}
 }
 
-func (svr *Service) FilePutContents(filename string, value string) error {
-	realFilename := svr.realConfigFileName(filename)
+func (s *Service) FilePutContents(filename string, value string) error {
+	realFilename := s.realConfigFileName(filename)
 	if !file.Exists(realFilename) {
 		if _, err := file.Create(realFilename); err != nil {
 			return err
 		}
 	}
-	if err := os.WriteFile(realFilename, []byte(value), file.DefaultPermOpen); err != nil {
+	if err := ioutil.WriteFile(realFilename, []byte(value), file.DefaultPermOpen); err != nil {
 		return err
 	}
 	return nil
 }
 
-func (svr *Service) ConfigFileStat(filename string) (os.FileInfo, error) {
-	return os.Stat(svr.realConfigFileName(filename))
+func (s *Service) ConfigFileStat(filename string) (os.FileInfo, error) {
+	return os.Stat(s.realConfigFileName(filename))
 }
 
-func (svr *Service) realConfigFileName(filename string) string {
-	return filepath.Join(svr.releasePath, filename)
+func (s *Service) realConfigFileName(filename string) string {
+	return filepath.Join(s.releasePath, filename)
+}
+
+// getClient 获取 zrpc 客户端
+func (s *Service) getClient(target string) (zrpc.Client, error) {
+	return s.getResource(s.getClientKey(target), func() (zrpc.Client, error) {
+		logx.Info("Init zrpc client:", target)
+		return zrpc.NewClientWithTarget(target)
+	})
+}
+
+func (s *Service) getClientKey(target string) string {
+	return fmt.Sprintf("%s%s", rpcClientPrefix, target)
+}
+
+func (s *Service) getResource(key string, create func() (zrpc.Client, error)) (zrpc.Client, error) {
+	val, err := s.shareCall.Do(key, func() (interface{}, error) {
+		s.lock.RLock()
+		client, ok := s.resource[key]
+		s.lock.RUnlock()
+		if ok {
+			return client, nil
+		}
+		val, err := create()
+		if err != nil {
+			return nil, err
+		}
+
+		s.lock.Lock()
+		s.resource[key] = val
+		s.lock.Unlock()
+		return val, err
+	})
+	if err != nil {
+		return nil, err
+	}
+	return val.(zrpc.Client), nil
 }
--- service/agent/biz/releasekv/service_test.go
+++ service/agent/biz/releasekv/service_test.go
@@ -16,3 +16,14 @@ func TestFilePutContents(t *testing.T) {
 	}
 	os.RemoveAll(configPath)
 }
+
+func TestGetClient(t *testing.T) {
+	configPath, _ := filepath.Abs("config")
+	s := NewService(configPath)
+	for i := 0; i < 10; i++ {
+		_, err := s.getClient("127.0.0.1:8080")
+		if err != nil {
+			t.Fatalf("Get client err: %s", err.Error())
+		}
+	}
+}
